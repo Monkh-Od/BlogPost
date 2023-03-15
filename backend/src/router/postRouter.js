98a5ee40a1a79fe5ce59025b6369ee999a6b45bd
WhyOni <paaki2468@mail.com>
2023-03-15 16:47:38 +0800

    fixed display blogposts

--- backend/src/router/postRouter.js
+++ backend/src/router/postRouter.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const { getPosts, createPost, deletePost, updatePost, getAllPosts } = require("../controller/postControl");
+const {
+  getPosts,
+  createPost,
+  deletePost,
+  updatePost,
+  getAllPosts,
+  getPostById,
+} = require("../controller/postControl");
 
 const postRouter = express.Router();
 
 postRouter
-.get("/getposts/:user", getPosts)
-.get("/allposts/:limit/:page", getAllPosts)
-.post("/createPost", createPost)
-.delete("/deletePost/:id", deletePost)
-.put("/updatePost/:id", updatePost)
+  .get("/getposts/:user", getPosts)
+  .get("/getpost/:postId", getPostById)
+  .get("/allposts/:limit/:page", getAllPosts)
+  .post("/createPost", createPost)
+  .delete("/deletePost/:id", deletePost)
+  .put("/updatePost/:id", updatePost);
 
 module.exports = postRouter;
-    
