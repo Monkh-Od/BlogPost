42a4def62adac228fb6cbdb670b2b4f0449350fb
22-HOP2-Munx.od <22-hop2-munx.od@Mstarslab402s-iMac.local>
2023-03-17 16:23:58 +0800

    contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { Layout } from "./layout/Layout";
 import { BlogPost, HomePage, Post, Signin, Signup } from "./pages";
 import { AuthProvider, ThemeContext, UserInfoContext } from "./contexts";
 
+import React from "react";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +20,7 @@ function App() {
                 <Route path="/blogposts" element={<BlogPost />} />
                 <Route path="/blogposts/:postId" element={<Post />} />
                 <Route path="/blog/postcreate" element={<PostCreate />} />
+                <Route path="/contact" element={<Contact />} />
               </Routes>
             </Layout>
           </ThemeContext>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,3 +3,4 @@ export * from "./Signin";
 export * from "./Signup";
 export * from "./Home";
 export * from "./Postpage";
+export * from "./Contact"
